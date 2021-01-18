7cf5b479f9c4c7aa705414c6160077d4d2136cf2
黑色的白兔子 <0500aa@protonmail.com>
2021-01-18 12:55:14 +0800

    Create a server order

---
+++ order/src/main/java/pers/bwr/learn/springcloud/orderingsystem/controller/OrderHandler.java
@@ -0,0 +1,20 @@
+package pers.bwr.learn.springcloud.orderingsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/order")
+public class OrderHandler {
+
+    @Value("$server.port")
+    private String port;
+
+    @GetMapping("/index")
+    public String index() {
+        return "order的端口：" + this.port;
+    }
+
+}
+
